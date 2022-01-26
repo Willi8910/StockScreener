1218a15d954390c7e6f7d18badb6cc55cf8d2510
William Lie <williamlie8910@gmail.com>
2022-01-26 10:18:52 +0700

    Add Rubocop configuration

--- Gemfile
+++ Gemfile
@@ -1,21 +1,25 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby "3.0.3"
+ruby '3.0.3'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.0.1"
+gem 'rails', '~> 7.0.1'
 
 # Use sqlite3 as the database for Active Record
-gem "pg"
 gem 'devise'
 gem 'devise-jwt'
 gem 'fast_jsonapi'
+gem 'pg'
 gem 'rack-cors'
-
+gem 'rubocop', '~> 1.25'
+gem 'selenium-webdriver', '~> 3.142', '>= 3.142.7'
+gem 'ffi'
 
 # Use the Puma web server [https://github.com/puma/puma]
-gem "puma", "~> 5.0"
+gem 'puma', '~> 5.0'
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
@@ -30,10 +34,10 @@ gem "puma", "~> 5.0"
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
-gem "bootsnap", require: false
+gem 'bootsnap', require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
@@ -43,11 +47,10 @@ gem "bootsnap", require: false
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem 'debug', platforms: %i[mri mingw x64_mingw]
 end
 
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,52 +1,55 @@
-class StocksController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_stock, only: %i[ show update destroy ]
+# frozen_string_literal: true
 
-    # GET /stocks
-    def index
-      @stocks = Stock.all
-  
-      render json: @stocks
+class StocksController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :set_stock, only: %i[show update destroy]
+
+  # GET /stocks
+  def index
+    @stocks = Stock.all
+
+    render json: @stocks
+  end
+
+  # GET /stocks/1
+  def show
+    render json: @stock
+  end
+
+  # POST /stocks
+  def create
+    @stock = Stock.new(stock_params)
+
+    if @stock.save
+      render json: @stock, status: :created, location: @stock
+    else
+      render json: @stock.errors, status: :unprocessable_entity
     end
-  
-    # GET /stocks/1
-    def show
+  end
+
+  # PATCH/PUT /stocks/1
+  def update
+    if @stock.update(stock_params)
       render json: @stock
+    else
+      render json: @stock.errors, status: :unprocessable_entity
     end
-  
-    # POST /stocks
-    def create
-      @stock = Stock.new(stock_params)
-  
-      if @stock.save
-        render json: @stock, status: :created, location: @stock
-      else
-        render json: @stock.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # PATCH/PUT /stocks/1
-    def update
-      if @stock.update(stock_params)
-        render json: @stock
-      else
-        render json: @stock.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # DELETE /stocks/1
-    def destroy
-      @stock.destroy
-    end
-  
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_stock
-        @stock = Stock.find(params[:id])
-      end
-  
-      # Only allow a list of trusted parameters through.
-      def stock_params
-        params.fetch(:stock, {}).permit(:name, :value, :pb_fair_value, :pe_fair_value, :benjamin_fair_value)
-      end
+  end
+
+  # DELETE /stocks/1
+  def destroy
+    @stock.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_stock
+    @stock = Stock.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def stock_params
+    params.fetch(:stock, {}).permit(:name, :value, :pb_fair_value, :pe_fair_value, :benjamin_fair_value)
+  end
 end
